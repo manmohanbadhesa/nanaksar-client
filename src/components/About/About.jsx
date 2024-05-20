b296c8229974fa9504931edd00e9c2cdd73b5053
manmohanbadhesa <manmohanb1995@gmail.com>
2024-05-20 16:27:11 -0700

    homepage fixes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,7 +20,31 @@ function About() {
                 <div className="about__sevadaar">
                     <img src={BhaiKanhaiyaJi} className="about__sevadaar-img"></img>
                     <div className="about__seva">
-                        seva
+                        <div className="about__seva-heading">
+                            <h1 className="about__seva__title">
+                                Seva
+                            </h1>
+                            
+                        </div>
+                        <div className="about__seva-bhai-kanhaiya">
+
+                        </div>
+                        <div className="about__seva-bhangti">
+                            <p className="about__seva-bhangti--bani">ਇਤੁ ਤਨਿ ਲਾਗੈ ਬਾਣੀਆ || </p>
+                            <p className="about__seva-bhangti--transliteration">This body is softened with the Word of the Guru's Bani</p>
+                            <p className="about__seva-bhangti--bani">ਸੁਖੁ ਹੋਵੈ ਸੇਵ ਕਮਾਣੀਆ ||</p>
+                            <p className="about__seva-bhangti--transliteration">You shall find peace, doing seva.</p>
+                            <p className="about__seva-bhangti--bani">ਸਭ ਦੁਨੀਆ ਆਵਣ ਜਾਣੀਆ</p>
+                            <p className="about__seva-bhangti--transliteration">All the world continues coming and going in reincarnation.</p>
+                            <p className="about__seva-bhangti--bani">ਵਿਚਿ ਦੁਨੀਆ ਸੇਵ ਕਮਾਈਐ ||</p>
+                            <p className="about__seva-bhangti--transliteration">In the midst of this world, do seva.</p>
+                            <p className="about__seva-bhangti--bani">ਤਾ ਦਰਗਹ ਬੈੈਸਣੁ ਪਾਈਐ ||</p>
+                            <p className="about__seva-bhangti--transliteration">And you shall be given a place of honor in the Court of the Lord</p>
+                            <p className="about__seva-bhangti--bani">ਕਹੁ ਨਾਨਕ ਬਾਹ ਲੁਡਾਈਐ ||</p>
+                            <p className="about__seva-bhangti--transliteration">Says Nanak, swing your arms in joy!</p>
+
+                        </div>
+
                     </div>
                 </div>
             </section>
